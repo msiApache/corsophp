b2e9272a78e329a036ecfdc3f93771e057281d86
gianlucatuono <gianlucatuono@libero.it>
2020-06-22 09:53:23 +0200

    Allineate funzioni e stampe

--- src/OPERATORS/index.php
+++ src/OPERATORS/index.php
@@ -1,1 +1,24 @@
 <?php
+/**OPERATORI DI ASSEGNAZIONE*/
+
+$show = true;
+
+if ($show) {
+    $a = 5;
+    $b = 8;
+
+    $c = $a + $b;
+    echo '5+8 = ' . $c;
+
+    /**PRECENDEZA DELLE OPERAZIONI*/
+    $c = $a + $b * 5; /*OUTPUT -> 45*/
+    $c1 = ($a + $b) * 5;/*OUTPUT -> 65*/
+    echo '</br>' . '5+8*5 = '.$c . '</br>' . '(5+8)*5 = '.$c1;
+
+    /**MODULO DELLA DIVISIONE*/
+    $f = 25 % 8;
+    echo '</br>'. '25 % 8 = ' . $f;
+
+}
+
+
--- src/VARIABLES/index.php
+++ src/VARIABLES/index.php
@@ -1,22 +1,22 @@
 <?php
 /**TUTTO CIO' CHE E' NELLO SHOW CON VALORE IMPOSTATO A FALSE NON E' VISIBILE*/
-$show = false;
+$show = true;
 
 if ($show) {
 
     $_ = 'test';
-    echo $_;
+    echo 'stampa contenuto variabile = ' . $_;
     /** VALORE FALSE : FALSE - 0 - [] - null */
     $isLoaded = false;
     if ($isLoaded) {
-        echo 'arma carica';
+        echo '<br>' . 'arma carica';
     } else {
-        echo 'arma non carica';
+        echo '<br>' . 'la variabile condizionata $isLoaded assume un valore di false = ' . 'arma non carica';
     }
 
     $isLoaded2 = 1;
     if ($isLoaded2) {
-        echo '<br/>arma carica';
+        echo '<br/>' . 'la variabile condizionata $isLoaded2 assume un valore di true = ' . 'arma carica';
     } else {
         echo '<br/>arma non carica';
     }
@@ -24,7 +24,7 @@ if ($show) {
     /**LE VARIABILI VENGONO RICONOSCIUTE SONO TRA APICI DOPPI LA STESSA COSA VALORE PER NEW LINE \n */
     $name = 'Gianluca';
     $lastName = "$name Tuono";
-    $lastName2 = '<br/>$name Tuono';
+    $lastName2 = '$name Tuono';
     $lastName3 = "$name Tuono";
     $objClass = new stdClass();
 
@@ -40,26 +40,22 @@ VIRGOLETTE;
 $lastName3
 VIRGOLETTE;
 
-    $doc3 = <<<'VIRGOLETTE'
-$lastName3
-VIRGOLETTE;
+    $objClass->custom = 'customVaraiabes';
 
-    $objClass->custom = '<br/>customVaraiabes';
-
-    $address = "Corso Racconigi";
+    $address = "Corso Racconigi ";
     $accounts['accountNumber'] = 5436373693;
 
-    echo "$lastName $doc $objClass->custom";
-    echo "<br/>$address{$accounts['accountNumber']}";
-    echo "<br/>$doc2 <br/>$doc3";
-
+    echo '<br/>stampa la variabile $lastName, composta dalla stringa $nameTuono = ' . $lastName;
+    echo '<br/>stampa la variabile $doc che non assume il valore Gianluca perchè tra apici singoli e non doppi = ' . $doc;
+    echo '<br/>stampa l\'oggetto di classe della variabile $objClass->custom = ' . $objClass->custom;
+    echo "<br/>stampa il contenuto di una variabile e un array " . '$address{$accounts[\'accountNumber\']} = ' . "$address{$accounts['accountNumber']}";
 
     /**IN PHP ANCHE LE STRINGHE VENGONO TRATTATE COME ARRAY OGNI CARATTERE CORRISPONDE AD UN INDICE LE LETTERE ACCENTATE
      * OCCUPANO DUE CELLE DI MEMORIA*/
     $name = 'Hydran';
     $accentend = 'à';
 
-    echo $name[0], $name[1], $name[2], $name[3], $accentend, $name[5];
+    echo '<br/>php tratta le stringhe come fossero array la variabile $name contiene il nome Hydran, sostituendo la a\' alla a indice[4] = ' . $name[0], $name[1], $name[2], $name[3], $accentend, $name[5];
 
     define('giallo', 4);
     /*$arr = array();
@@ -73,16 +69,14 @@ VIRGOLETTE;
     $arr[4] = [1, 2, 3, 4];
     $arr['giorni'] = ['LUNEDI', 'MARTEDI', 'MERCOLDI', 'GIOVEDI', 'VENERDI', 'SABATO', 'DOMENICA'];
     $arr['mesi'] = ['G', 'F'];
-    /**ELIMINA IL CONTENUTO DELL'ARRAY KEY VALUE*/
-    unset($arr['mesi']);
-//echo $arr[giallo];
-//var_dump($arr['giorni'][0]);
-    var_dump($arr);
-    var_dump($arr['mesi']);
+    /** unset() ELIMINA IL CONTENUTO DELL'ARRAY KEY VALUE*/
+    //unset($arr['mesi']);
+    echo '<br>stampa array $arr = '; var_dump($arr);
+    echo '<br>stampa array associativo $arr[\'mesi\'] = '; var_dump($arr['mesi']);
     /**LA FUNZIONE array_values resistuisce il valore dell'array copiato ordinando gli indici dell'array*/
     $arr2 = [];
     $arr2 = array_values($arr);
-    var_dump($arr2);
+    echo '<br>stampa array $arr2 assegnatoli il valore dell\'array $arr mediante la funzione  $arr2 = array_values($arr); = '; var_dump($arr);
 }
 
 
